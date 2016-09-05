6f5c50b5525e1ccf09388c573e08dae4c642e17d
afrolionx <mike@mikecorp.ca>
2016-09-05 18:43:20 -0400

    created helpers module for all routes

---
+++ routes/helpers.js
@@ -0,0 +1,10 @@
+'use strict';
+const Playlist = require('../models/playlist');
+
+module.exports.createPlaylistItems = function createPlaylistItems (items) {
+  let playlists = [];
+  items.forEach((item) => {
+    playlists.push(new Playlist(item).render());
+  });
+  return playlists;
+};
