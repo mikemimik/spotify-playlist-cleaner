b0efae641c64907386678c61a002b58114c80103
afrolionx <mike@mikecorp.ca>
2016-09-20 15:13:30 -0400

    refactored search route

--- routes/search.js
+++ routes/search.js
@@ -1,45 +1,26 @@
 'use strict';
-const AppState = require('../src/state');
+
+const requireAuth = require('../utils/middleware-auth');
 const Handlers = require('../handlers');
 const express = require('express');
+const spotify = require('../src/spotify-api');
 const Helpers = require('./helpers');
 const router = express.Router();
 
 router.route('/')
-  .all((req, res, next) => {
-    // check app state
-    if (AppState.render.user) {
-      AppState.render.currentUrl = '/search';
-      next();
-    } else {
-      AppState.render.currentUrl = '/';
-      AppState.render.notification = {
-        type: 'warning',
-        message: 'Need to be logged in!'
-      };
-      res.redirect('/');
-    }
-  })
+  .get(requireAuth)
   .get((req, res, next) => {
-    res.render(
-      'search',
-      AppState.render,
-      Helpers.renderView.bind(this, res, AppState)
-    );
+    res.render('search');
   })
   .post((req, res, next) => {
-    console.log('received POST to /search'); // TESTING
     // console.log('body:', req.body); // TESTING
     // let search = req.body.searchInput;
-    AppState.spotifyWebApi.getUserPlaylists(AppState.render.user.id)
+    spotify.WebApi.getUserPlaylists(req.session.user.id)
       .then((data) => {
         // console.log(data.body.items); // TESTING
-        AppState.render.playlists = Helpers.createPlaylistItems(data.body.items);
-        res.render(
-          'results',
-          AppState.render,
-          Helpers.renderView.bind(this, res, AppState)
-        );
+        res.locals.playlists = Helpers.createPlaylistItems(data.body.items);
+        spotify.WebApi.resetAccessToken();
+        res.render('results');
       })
       .catch(Handlers.error);
   });
