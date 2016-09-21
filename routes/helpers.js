1c9929abe01248e699130f89a823d7e9ab1d7675
afrolionx <mike@mikecorp.ca>
2016-09-21 16:28:42 -0400

    refactored route helper module

--- routes/helpers.js
+++ routes/helpers.js
@@ -1,19 +1,11 @@
 'use strict';
-const Handlers = require('../handlers');
+
 const Playlist = require('../models/playlist');
 
 module.exports.createPlaylistItems = function createPlaylistItems (items) {
   let playlists = [];
   items.forEach((item) => {
-    playlists.push(new Playlist(item).render());
+    playlists.push(new Playlist(item));
   });
   return playlists;
-};
-
-module.exports.renderView = function renderView (res, state, err, html) {
-  if (err) { Handlers.error.call(err); }
-  if (state.render.notification) {
-    delete state.render.notification;
-  }
-  res.send(html);
 };
