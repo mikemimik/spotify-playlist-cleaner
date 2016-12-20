54095242d18f618b593990af558ee2d8ceeff350
afrolionx <mike@mikecorp.ca>
2016-12-20 01:02:47 -0500

    removed functions from routes/helpers.js - this file should be for general routes

--- routes/helpers.js
+++ routes/helpers.js
@@ -1,19 +1,12 @@
+// FILE: RouteHelper
 'use strict';
-
-const Models = require('../models');
-
-module.exports.createPlaylistItems = function createPlaylistItems (items) {
-  return items.map((item) => {
-    return new Models.Playlist(item);
-  });
-};
-
-module.exports.createTrackItems = function createTrackItems (items) {
-  return items.map((item, index) => {
-    return new Models.Track(item.track, index);
-  });
-};
 
 module.exports.createPaginationObj = function createPaginationObj () {
 
+};
+
+module.exports.LogRouteInput = function LogRouteInput (req) {
+  console.log('params:', req.params);
+  console.log('query:', req.query);
+  console.log('body:', req.body);
 };
